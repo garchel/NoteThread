802c469de5b7f5b22676c71c4f740fed0c310a47
garchel <paulovictor.co97@gmail.com>
2026-08-21 13:18:21 -0300

    test: sw v17

--- tests/sync.test.js
+++ tests/sync.test.js
@@ -17,8 +17,8 @@ describe('PWA', ()=>{
     assert.equal(m.icons.length,4);
     assert.ok(m.icons.find(i=>i.sizes==='1024x1024'));
   });
-  it('sw.js é v16', ()=>{
+  it('sw.js é v17', ()=>{
     const sw=readFileSync('public/sw.js','utf8');
-    assert.match(sw, /notethread-v16/);
+    assert.match(sw, /notethread-v17/);
   });
 });
